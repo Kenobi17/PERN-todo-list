a56a7008ed5c851283b4290199a5959349b4a986
Kenobi17 <matiascorrea420@gmail.com>
2020-09-25 17:18:15 -0300

    Add components directory and InputTodo.jsx component

---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from "react";
+import TodoAPI from "../apis/TodoAPI";
+import { TodoContext } from "../context/TodoContext";
+
+const InputTodo = () => {
+  const { addTodo } = useContext(TodoContext);
+  const [description, setDescription] = useState("");
+  const handleChange = (e) => {
+    const { value } = e.currentTarget;
+    setDescription(value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await TodoAPI.post("/", {
+        description: description,
+      });
+      addTodo(response.data.data.todo);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <div className="InputTodo">
+      <h1 className="text-center my-5">Input Todo</h1>
+      <form onSubmit={handleSubmit} className="d-flex">
+        <input
+          onChange={handleChange}
+          value={description}
+          name="description"
+          type="text"
+          placeholder="add to do"
+          className="form-control"></input>
+        <button type="submit" className="btn btn-success">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputTodo;
