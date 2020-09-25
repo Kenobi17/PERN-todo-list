1987a403e74a52dec587174b53ce6400c833cd10
Kenobi17 <matiascorrea420@gmail.com>
2020-09-25 16:36:44 -0300

    Change App.js to App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  return (
+    <div className="App container">
+      <InputTodo />
+    </div>
+  );
+}
+
+export default App;
